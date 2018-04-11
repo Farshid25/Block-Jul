6296a9ee58cb3cfc408a670f14249ca3ad849339
aversio <aversio@protonmail.com>
2018-04-11 15:59:13 +0200

    Added layout and patient add function

--- TheJulius/wwwroot/functies.js
+++ TheJulius/wwwroot/functies.js
@@ -6,30 +6,32 @@
 	success: function (data) {
 
 		$.each(data, function (index, user) {
-			$("#lijstUsers").append(
-				"<p>" +
-				"<b>id: </b>" + user.id + "</br>" +
-				"<b>bsn: </b>" + user.age + "</br>" +
-				"<b>title: </b>" + user.name + "</br>" +
-				"</p>"
+            $("#tablebody").append(
+                "test" + 
+                "<tr>" +
+                    "<td>" + user.id + "</td>" +
+                    "<td>" + user.age + "</td>" +
+                    "<td>" + user.name + "</td>" +
+                "</tr>"
 			);
 		});
 	}	
 });
  
 $(document).ready(function () {
-	$("#send").click(function () {
+	$("#register").click(function () {
 		var age = $('#age').val();
 		var name = $('#name').val();
 		alert(age + " " + name);
 		$.ajax({
-			type: "POST",
-			url: "/api/User",
-			data: "/api/User?Age=" + age + "&Name=" + name,
-			success: function (msg) {
-				alert("Data Saved: " + msg);
-			}
-		});
-	});
+            type: "POST",
+            url: "/api/User",
+            dataType: 'json',
+            data: "Age=" + age + "&Name=" + name,
+            success: function (msg) {
+                alert("Data Saved: " + msg);
+            }
+        });
+    });
 });
 
